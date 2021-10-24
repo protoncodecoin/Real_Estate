31fafd540f482c5838aae8ad69eea26cb870b325
Prince Asante <sistercharmings@gmail.com>
2021-10-24 10:28:11 -0700

    created and added new funcionality to the listings.urls.py

--- btre/urls.py
+++ btre/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('listing/', include('listings.urls')),
     path('', include('pages.urls'))
 ]
