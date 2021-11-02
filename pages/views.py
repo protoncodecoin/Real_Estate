e1a69848806087114f96e93885dbede47a4d0146
princeprince <sistercharmings@gmail.com>
2021-11-02 19:02:02 +0000

    passed the listing models into index function to display data to the homepage

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
 
 def index(request):
-#    context = "<h1> Home Page</h1>"
-    return render (request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings
+
+    }
+    return render (request, 'pages/index.html', context)
 
 
 def about(request):
-#    context = "<h1> About Page </h1>"
     return render (request, 'pages/about.html')
