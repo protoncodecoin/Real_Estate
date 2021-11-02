f013b1dabaa4dc5d2984f6cd44f1e5a154cc5c9d
princeprince <sistercharmings@gmail.com>
2021-11-02 13:03:00 +0000

    added the root of the database to settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btre',
+        'USER': 'postgres',
+        'PASSWORD': 'sistercharmings@531778',
+        'HOST': 'localhost'
     }
 }
 
