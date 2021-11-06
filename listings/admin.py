439a9dd590df0d51c05b313ada968d4c58767c3c
princeprince <sistercharmings@gmail.com>
2021-11-06 15:11:59 +0000

    created the choices.py for the search data and modified the about, admin.py and admin.css files

--- listings/admin.py
+++ listings/admin.py
@@ -6,7 +6,7 @@ class ListingAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'title')
     list_filter = ('realtor',)
     list_editable = ('is_published',)
-    search_fields = ('title', 'address', 'city', 'realtor')
+    search_fields = ('title', 'address')
 
     list_per_page = 25
 admin.site.register(Listing, ListingAdmin)
